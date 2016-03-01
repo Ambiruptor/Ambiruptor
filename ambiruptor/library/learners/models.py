97b427f0befc1f7801d5e23fb7e7cfa6d83ae600
Eugenia <yarutoua@gmail.com>
2016-03-01 19:39:08 +0100

    Added: new models SVM RBF, Naive Bayes, Decision Tree classifiers

--- ambiruptor/library/learners/models.py
+++ ambiruptor/library/learners/models.py
@@ -1,20 +1,33 @@
 """
 A collection of classes implementing learning models for diasmbiguation
-
+- SVM Classifier with Linear kernel
+(http://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html)
+- SVM Classifier with Radial Basis Function kernel
+(http://scikit-learn.org/stable/auto_examples/svm/plot_rbf_parameters.html#example-svm-plot-rbf-parameters-py)
+- Gaussian Naive Bayes Classifier
+(http://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.GaussianNB.html#sklearn.naive_bayes.GaussianNB)
+- Decision Tree Classifier
+(http://scikit-learn.org/stable/modules/tree.html#classification)
+- Forests of randomized trees
+(http://scikit-learn.org/stable/modules/ensemble.html#forests-of-randomized-trees)
 """
 
 import numpy as np
 import pickle
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVC, SVC
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 
 from ambiruptor.base.core import Learner
 
 
+# Parametrized by 'C' - Penalty parameter of the error term.
 class LinearSVMClassifier(Learner):
-    def __init__(self):
+    def __init__(self, C=1.0):
         """Init the learning model"""
-        self.model = OneVsRestClassifier(LinearSVC())
+        self.model = OneVsRestClassifier(LinearSVC(C=C))
 
     def train(self, train_data):
         """Train the model using the training set"""
@@ -24,4 +37,61 @@ class LinearSVMClassifier(Learner):
         return self.model.predict(ambiguous_data.data)
 
 
-# TODO : New models...
+# Parametrized by
+# 'C' that trades off misclassification of training examples against simplicity of the decision surface
+# 'gamma' that defines how far the influence of a single training example reaches
+# model parameters
+class RbfSVMClassifier(Learner):
+    def __init__(self, C=1.0, gamma='auto'):
+        """Init the learning model"""
+        self.model = OneVsRestClassifier(SVC(C=C, gamma=gamma, kernel='rbf'))
+
+    def train(self, train_data):
+        """Train the model using the training set"""
+        self.model.fit(train_data.data, train_data.senses)
+
+    def predict(self, ambiguous_data):
+        return self.model.predict(ambiguous_data.data)
+
+
+class NaiveBayesClassifier(Learner):
+    def __init__(self):
+        """Init the learning model"""
+        self.model = OneVsRestClassifier(GaussianNB())
+
+    def train(self, train_data):
+        """Train the model using the training set"""
+        self.model.fit(train_data.data, train_data.senses)
+
+    def predict(self, ambiguous_data):
+        return self.model.predict(ambiguous_data.data)
+
+
+class DecisionTreeClassifier(Learner):
+    def __init__(self):
+        """Init the learning model"""
+        self.model = OneVsRestClassifier(tree.DecisionTreeClassifier())
+
+    def train(self, train_data):
+        """Train the model using the training set"""
+        self.model.fit(train_data.data, train_data.senses)
+
+    def predict(self, ambiguous_data):
+        return self.model.predict(ambiguous_data.data)
+
+
+# Parametrized with number of estimators
+# default 5 - ?
+class RandomForestClassifier(Learner):
+    def __init__(self, n_estimators=None):
+        """Init the learning model"""
+        self.model = RandomForestClassifier(n_estimators)
+
+    def train(self, train_data):
+        """Train the model using the training set"""
+        self.model.fit(train_data.data, train_data.senses)
+
+    def predict(self, ambiguous_data):
+        return self.model.predict(ambiguous_data.data)
+
+
--- tests/test.py
+++ tests/test.py
@@ -4,15 +4,21 @@ import re
 import time
 
 from ambiruptor.library.preprocessors.data_structures import TrainData
+import ambiruptor.library.preprocessors.feature_extractors as fe
+
 from ambiruptor.library.learners.models import LinearSVMClassifier
+from ambiruptor.library.learners.models import RbfSVMClassifier
+from ambiruptor.library.learners.models import NaiveBayesClassifier
+from ambiruptor.library.learners.models import DecisionTreeClassifier
+from ambiruptor.library.learners.models import RandomForestClassifier
+
 from ambiruptor.library.miners.wiki_miners import DataMining
 
-import ambiruptor.library.preprocessors.feature_extractors as fe
 
 if __name__ == '__main__':
 
     # Data Mining
-    print("************************** Data mining ****************************")
+    print("************************** Data mining ***************************")
     t = time.time()
     data = DataMining()
     data.set_wikidump_filename("data/wikidump.xml")
@@ -21,30 +27,30 @@ if __name__ == '__main__':
     corpus = data.get_corpus("Bar_(disambiguation)")
     print("Size of the corpus:", len(corpus), "articles")
     print("Done,", time.time() - t, "s")
-    
+
     # Building features
-    print("********************** Building features **************************")
+    print("********************** Building features *************************")
     t = time.time()
     feature1 = fe.PartOfSpeechFeatureExtractor()
     feature2 = fe.CloseWordsFeatureExtractor()
     feature2.build_typicalwords(corpus)
     print("Done,", time.time() - t, "s")
-    
+
     # Feature extraction (corpus)
-    print("******************* Feature extraction (corpus) *******************")
+    print("******************* Feature extraction (corpus) ******************")
     t = time.time()
     corpus_extractor = fe.CorpusExtraction()
     corpus_extractor.add_feature(feature2)
     train_data = corpus_extractor.extract_features(corpus)
     print("Shape of the matrix of features:", train_data.data.shape)
     print("Done,", time.time() - t, "s")
-    
+
     # Feature extraction (ambiguous text)
-    print("************** Feature extraction (ambiguous text) ****************")
+    print("************** Feature extraction (ambiguous text) ***************")
     t = time.time()
     ambiguous_extractor = fe.AmbiguousExtraction()
     ambiguous_extractor.add_feature(feature2)
-    
+
     text = """The bar of a mature tropical cyclone is a very dark gray-black
               layer of cloud appearing near the horizon as seen from an observer
               preceding the approach of the storm, and is composed of dense
@@ -54,16 +60,53 @@ if __name__ == '__main__':
               usually visible in ascending order above the top of the bar, while
               the wind direction for an observer facing toward the bar is
               typically from the left and slightly behind the observer."""
-    
+
     ambiguous_data = ambiguous_extractor.extract_features(text, "bar")
     print("Shape of the matrix of features:", ambiguous_data.data.shape)
     print("Done,", time.time() - t, "s")
-    
+
+    print(ambiguous_data.data)
+    print(ambiguous_data.targets)
+    print(ambiguous_data.words)
+
     # Learning model
-    print("************************* Learning model **************************")
+    print("************************* Learning model *************************")
+    print("-- SVM with Linear Kernel (default parameters")
     t = time.time()
     model = LinearSVMClassifier()
     model.train(train_data)
     labels = model.predict(ambiguous_data)
     print(labels)
-    print("Done,", time.time() - t, "s")
+    print("Done,", time.time() - t, "s\n\n")
+
+    print("-- SVM with RBF Kernel (default parameters")
+    t = time.time()
+    model = RbfSVMClassifier()
+    model.train(train_data)
+    labels = model.predict(ambiguous_data)
+    print(labels)
+    print("Done,", time.time() - t, "s\n\n")
+
+    print("-- Naive Bayes Classifier")
+    t = time.time()
+    model = NaiveBayesClassifier()
+    model.train(train_data)
+    labels = model.predict(ambiguous_data)
+    print(labels)
+    print("Done,", time.time() - t, "s\n\n")
+
+    print("-- Decision Tree Classifier")
+    t = time.time()
+    model = DecisionTreeClassifier()
+    model.train(train_data)
+    labels = model.predict(ambiguous_data)
+    print(labels)
+    print("Done,", time.time() - t, "s\n\n")
+
+    # print("-- Random Forest Classifier (default parameters)")
+    # t = time.time()
+    # model = RandomForestClassifier()
+    # model.train(train_data)
+    # labels = model.predict(ambiguous_data)
+    # print(labels)
+    # print("Done,", time.time() - t, "s\n\n")
