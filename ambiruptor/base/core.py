ccf08652f6de801251482f3839edcc9578cab852
Eugenia <yarutoua@gmail.com>
2016-03-02 22:29:04 +0100

    Modified: added some functions to Learner base class to implement scikit-learn interface

--- ambiruptor/base/core.py
+++ ambiruptor/base/core.py
@@ -1,7 +1,7 @@
-""" Core base classes """
+"""Core base classes"""
 
 from abc import ABCMeta, abstractmethod
-from nltk import word_tokenize
+import pickle
 
 
 class DocStringInheritor(ABCMeta):
@@ -29,86 +29,112 @@ class DocStringInheritor(ABCMeta):
         return ABCMeta.__new__(meta, name, bases, clsdict)
 
 
-class Learner(object, metaclass=DocStringInheritor):
-    """Abstract class for learning"""
+class Learner(object):
+    """Abstract class for learning."""
+
+    __metaclass__ = DocStringInheritor
 
     def __init__(self):
-        """Init the learning model"""
+        """Init the learning model."""
+        pass
 
     @abstractmethod
     def train(self, train_data):
-        """Train the model using the training set
-        train_data -- instance of TrainData
+        """Train the model using the training set train_data -- instance of TrainData.
         """
+        pass
+
+    def fit(self, train_data):
+        """Provide the interface for scikit-learn utils
+        simply calling the train method.
+        """
+        return self.tain(train_data)
+
+    def score(self, ambiguous_data):
+        """Provide the scoring interface for scikit-learn utils."""
+        return self.model.score(ambiguous_data.data, ambiguous_data.targets)
+
+    def get_params(self, deep=True):
+        """Provide the get_params interface for scikit-learn utils."""
+        return self.model.get_params(deep)
 
     @abstractmethod
     def predict(self, data):
-        """Guess the best sense for a features vector
-        data -- instance of AmbiguousData
+        """Guess the best sense for a features vector data -- instance of
+        AmbiguousData.
         """
+        pass
 
     def load(self, filename):
-        """Load a model from a binary file"""
+        """Load a model from a binary file."""
         with open(filename, 'rb') as f:
             tmp_dict = pickle.loads(f)
         if tmp_dict is not None:
             self.__dict__ = tmp_dict
 
     def export(self, filename):
-        """Store a model into a binary file"""
+        """Store a model into a binary file."""
         with open(filename, 'wb') as f:
             pickle.dump(self.__dict__, f)
 
 
-class Miner(object, metaclass=DocStringInheritor):
-    """Abstract class for data mining"""
+class Miner(object):
+    """Abstract class for data mining."""
+
+    __metaclass__ = DocStringInheritor
 
     def __init__(self):
-        """Init"""
-    
+        """Init."""
+    pass
+
     @abstractmethod
     def build(self):
-        """Build a corpus"""
+        """Build a corpus."""
+        pass
 
     @abstractmethod
     def get_corpus(self, word):
-        """Get the corpus"""
+        """Get the corpus."""
+        pass
 
     def load(self, filename):
-        """Load a model from a binary file"""
+        """Load a model from a binary file."""
         with open(filename, 'rb') as f:
             tmp_dict = pickle.loads(f)
         if tmp_dict is not None:
             self.__dict__ = tmp_dict
 
     def export(self, filename):
-        """Store a model into a binary file"""
+        """Store a model into a binary file."""
         with open(filename, 'wb') as f:
             pickle.dump(self.__dict__, f)
 
 
-class FeatureExtractor(object, metaclass=DocStringInheritor):
-    """Abstract class for feature extraction"""
-    
+class FeatureExtractor(object):
+    """Abstract class for feature extraction."""
+
+    __metaclass__ = DocStringInheritor
+
     def __init__(self):
-        """Init the feature extractor"""
-    
+        """Init the feature extractor."""
+        pass
+
     @abstractmethod
     def extract_features(words, targets):
         """Extract feature vectors
         words -- vector of word
         targets -- list of target's indexes
-        return -- 2D-vector containing features"""
-    
+        return -- 2D-vector containing features.
+        """
+
     def load(self, filename):
-        """Load a model from a binary file"""
+        """Load a model from a binary file."""
         with open(filename, 'rb') as f:
             tmp_dict = pickle.loads(f)
         if tmp_dict is not None:
             self.__dict__ = tmp_dict
 
     def export(self, filename):
-        """Store a model into a binary file"""
+        """Store a model into a binary file."""
         with open(filename, 'wb') as f:
             pickle.dump(self.__dict__, f)
-
